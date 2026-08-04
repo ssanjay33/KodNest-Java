5fcf28c2d86f4597a412a636bba9b73a338d1db1
Your Name <spssanjay33@gmail.com>
2026-08-04 11:31:20 +0530

    P07

---
+++ track/M02/T01/ST01/P07/Main.java
@@ -0,0 +1,14 @@
+package track.M02.T01.ST01.P07;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
