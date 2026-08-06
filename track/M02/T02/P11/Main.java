0873b957a64d73e88201bf4ef5f0e1ec5a7181dc
Your Name <spssanjay33@gmail.com>
2026-08-06 12:47:41 +0530

    main.java

---
+++ track/M02/T02/P11/Main.java
@@ -0,0 +1,14 @@
+package track.M02.T02.P11;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (j == 2) {
+                    continue;
+                }
+                System.out.println("i : " + i + " j : " + j);
+            }
+        }
+    }
+}
